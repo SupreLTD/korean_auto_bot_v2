5196afc0db00de450504a0f8cdb9bb06bc831801
Vanya <balanaaarrrr@gmail.com>
2023-12-18 14:50:30 +0300

    handle price state machine

--- handlers/price_handler.py
+++ handlers/price_handler.py
@@ -1,8 +1,8 @@
 from aiogram import Router, types, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-
-# from magic_filter import F
+from aiogram.filters import MagicData
+from keyboards import price_kb as KB
 
 router = Router()
 
@@ -19,5 +19,62 @@ class FSMPrice(StatesGroup):
 async def get_age(call: types.CallbackQuery, state: FSMContext):
     await call.message.answer(
         '🚗 Возраст автомобиля?',
+        reply_markup=KB.age_choose()
     )
     await state.set_state(FSMPrice.age)
+
+
+@router.callback_query(KB.PriceChoose.filter(F.action == 'age'))
+async def get_engine(call: types.CallbackQuery, callback_data: KB.PriceChoose, state: FSMContext):
+    await state.update_data(age=callback_data.data)
+    await call.message.edit_text(f'🚗 Возраст автомобиля?\n\n✅ Ответ: {callback_data.answer}')
+    await call.message.answer('🚕 Тип двигателя?', reply_markup=KB.engine_choose())
+    await state.set_state(FSMPrice.engine)
+
+
+@router.callback_query(KB.PriceChoose.filter(F.action == 'engine'))
+async def get_power(call: types.CallbackQuery, callback_data: KB.PriceChoose, state: FSMContext):
+    await state.update_data(engine=callback_data.data)
+    await call.message.edit_text(f'🚕 Тип двигателя?\n\n✅ Ответ: {callback_data.answer}')
+    await call.message.answer('🏎 Мощность двигателя (л.с.)?')
+    await state.set_state(FSMPrice.power)
+
+
+@router.message(FSMPrice.power)
+async def get_capacity(message: types.Message, state: FSMContext):
+    if message.text.isdigit():
+        await state.update_data(power=message.text)
+        await message.answer(f'🏎 Мощность двигателя (л.с.)?\n\n✅ Ответ: {message.text} (л.с.)')
+        await message.answer('🚙 Объем двигателя см3 (1 л = 1000 см3)?')
+        await state.set_state(FSMPrice.capacity)
+    else:
+        await message.answer('Введите корректное значение!')
+    await message.delete()
+
+
+@router.message(FSMPrice.capacity)
+async def get_price(message: types.Message, state: FSMContext):
+    if message.text.isdigit():
+        await state.update_data(capacity=message.text)
+        await message.answer(f'🚙 Объем двигателя см3 (1 л = 1000 см3)??\n\n✅ Ответ: {message.text} (см3)')
+        await message.answer('💰 Стоимость автомобиля в Вонах (₩)')
+        await state.set_state(FSMPrice.price)
+    else:
+        await message.answer('Введите корректное значение!')
+    await message.delete()
+
+
+@router.message(FSMPrice.price)
+async def result(message: types.Message, state: FSMContext):
+    if message.text.isdigit():
+        fsm_data = await state.get_data()
+        age = fsm_data['age']
+        engine = fsm_data['engine']
+        power = fsm_data['power']
+        capacity = fsm_data['capacity']
+        price = message.text
+        # await message.answer(f'{age} {engine} {capacity} {power} {price}')
+
+    else:
+        await message.answer('Введите корректное значение!')
+    await message.delete()
--- keyboards/price_kb.py
+++ keyboards/price_kb.py
@@ -3,7 +3,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram import types
 
 
-class PriceChoose(CallbackData, prefix='p'):
+class PriceChoose(CallbackData, prefix='price'):
     action: str
     data: str
     answer: str
@@ -13,21 +13,21 @@ def age_choose():
     kb = InlineKeyboardBuilder()
     kb.row(
         types.InlineKeyboardButton(text='до 3 лет', callback_data=PriceChoose(
-            action='price',
+            action='age',
             answer='до 3 лет',
             data='0-3').pack()),
         types.InlineKeyboardButton(text='от 3 до 5 лет', callback_data=PriceChoose(
-            action='price',
+            action='age',
             answer='от 3 до 5 лет',
             data='3-5').pack())
     )
     kb.row(
         types.InlineKeyboardButton(text='от 5 до 7 лет', callback_data=PriceChoose(
-            action='price',
+            action='age',
             answer='от 5 до 7 лет',
             data='5-7').pack()),
         types.InlineKeyboardButton(text='старше 7 лет', callback_data=PriceChoose(
-            action='price',
+            action='age',
             answer='старше 7 лет',
             data='7-0').pack())
     )
@@ -39,21 +39,21 @@ def engine_choose():
     kb = InlineKeyboardBuilder()
     kb.row(
         types.InlineKeyboardButton(text='Бензин', callback_data=PriceChoose(
-            action='price',
+            action='engine',
             answer='Бензин',
             data='1').pack()),
         types.InlineKeyboardButton(text='Дизель', callback_data=PriceChoose(
-            action='price',
+            action='engine',
             answer='Дизель',
             data='2').pack())
     )
     kb.row(
         types.InlineKeyboardButton(text='Гибрид', callback_data=PriceChoose(
-            action='price',
+            action='engine',
             answer='Гибрид',
             data='3').pack()),
         types.InlineKeyboardButton(text='Электрический', callback_data=PriceChoose(
-            action='price',
+            action='engine',
             answer='Электрический',
             data='4').pack())
     )
